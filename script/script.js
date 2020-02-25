db77d6c6628b6fb02b83539528bd7e1c4656f8ec
Dmitry Likholetov <likholetov1990@gmail.com>
2020-02-25 18:55:47 +0300

    lesson19_full

--- script/script.js
+++ script/script.js
@@ -53,13 +53,21 @@ const toggleMenu = () => {
     const btnMenu = document.querySelector('.menu'),
             menu = document.querySelector('menu'),
             menuA = menu.querySelectorAll('a');
-
     const handlerMenu = () => {
             menu.classList.toggle('active-menu');
     };
-
+    menu.addEventListener('click', (event) => {
+        let target = event.target;
+        console.log(target);
+        if (target){
+            menuA.forEach((item) => {
+                if(item === target){
+                    handlerMenu();
+                }
+            });
+        }
+    });
     btnMenu.addEventListener('click', handlerMenu);
-    menuA.forEach((elem) => elem.addEventListener('click', handlerMenu));
 };
 toggleMenu();
 
